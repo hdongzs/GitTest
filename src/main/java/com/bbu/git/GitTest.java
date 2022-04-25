7e8676498cc909b65cddd921ca6152af79240e5c
hdongzs <104297618+hdongzs@users.noreply.github.com>
2022-04-25 14:37:12 +0800

    pull commit

--- src/main/java/com/bbu/git/GitTest.java
+++ src/main/java/com/bbu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello,git5,hot-fix");
         System.out.println("hello,git6 ,push");
         System.out.println("ssh hello,git6");
+        System.out.println("pull test!!!!!");
     }
 }
