1178945182fd1146aba2098a6bf8bcd3dbffefa0
hd <hd@qq.com>
2022-04-25 12:24:43 +0800

    second commit

--- src/main/java/com/bbu/git/GitTest.java
+++ src/main/java/com/bbu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.bbu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
